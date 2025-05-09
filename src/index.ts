45a32ad50aeac0df4b5eca343b9165b2e9fb2cc5
FelixAn <anfengxin@yondervision.com.cn>
2025-05-09 09:41:23 +0800

    feat: 移除调试信息

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { registerTools } from "./tools/register.js";
-import { searchFiles } from "./tools/search-files.js";
-import { enrichSearchResultsWithDefinitions } from "./tools/code-parser/index.js";
 
 // Create server instance
 const server = new McpServer({
@@ -23,13 +21,7 @@ async function main() {
   console.error("MCP Code Search Server running on stdio");
 }
 
-// main().catch((error) => {
-//   console.error("Fatal error in main():", error);
-//   process.exit(1);
-// });
-
-searchFiles("ydl.init.ajax", "/Users/anfengxin/Project/ydpx2ydap/demo/00000000/scripts", "*.js").then(async res => {
-  console.log(res)
-  const results = await enrichSearchResultsWithDefinitions(res, "ydl.init.ajax");
-  console.log(results)
-})
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
